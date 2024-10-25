e0c29d46752f7e8dde11b59b50e302eedd26f4a2
zacha <zachary.campbell2006@outlook.com>
2024-10-25 16:03:56 +1100

    added something, yet to support web

--- examples/new_display.rs
+++ examples/new_display.rs
@@ -1,6 +1,6 @@
 use eframe::{App, Frame, NativeOptions};
-use eframe::epaint::Rect;
-use egui::{Color32, Context, Painter, Pos2, Rounding, Stroke, Ui, Vec2, Window};
+use eframe::epaint::{Rect};
+use egui::{Align2, Color32, Context, FontId, Painter, Pos2, Response, Rounding, Sense, Stroke, TextBuffer, Ui, Vec2, Window};
 
 fn main() {
    let options = NativeOptions {
@@ -23,17 +23,28 @@ impl MyApp {
 impl App for MyApp {
    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
       Window::new("test")
+          .resizable(true)
           .show(ctx, |ui| {
              ui.label("Balls");
 
              ui.group(|ui| {
-                ui.set_max_size(Vec2::new(250.0, 150.0));
+                ui.set_max_size(ui.max_rect().size());
 
 
                 let data = vec![(0, [0.0, 250.0]), (1, [0.0, 150.0]), (1, [170.0, 200.0])];
                 test(ui, data);
              });
 
+          });
+
+      Window::new("")
+          .resizable(true)
+          .min_size(Vec2::ZERO)
+          .default_size(Vec2::new(200.0, 100.0))
+          .show(ctx, |ui| {
+             ui.allocate_space(ui.available_size());
+
+             let _resp = display_segment(ui, ui.max_rect(), "MAIN_UPDATE_INTERLOOP", 32.4, Color32::DARK_GRAY);
           });
    }
 }
@@ -74,12 +85,14 @@ fn test(ui: &mut Ui, data: Vec<(u32, [f32; 2])>) {
       for prof in data.iter() {
          let data_rect = rect_from_seg_x(prof.1[0], prof.1[1], prof.0, segmentation, rect);
 
-         painter.rect(
-            data_rect,
-            Rounding::same(5.0),
-            Color32::DARK_GRAY,
-            Stroke::new(1.0, Color32::GOLD),
-         );
+         // painter.rect(
+         //    data_rect,
+         //    Rounding::same(5.0),
+         //    Color32::DARK_GRAY,
+         //    Stroke::new(1.0, Color32::GOLD),
+         // );
+
+         display_segment(ui, data_rect, "TEST_NAME_PLACEHOLDER", 4.23, Color32::DARK_GRAY);
       }
    }
 }
@@ -106,4 +119,99 @@ fn compass(painter: &Painter, rect: &Rect) {
 
    painter.circle(rect.center() + Vec2::new(-25.0, 0.0), 5.0, Color32::GREEN, Stroke::default());
    painter.circle(rect.center() + Vec2::new(25.0, 0.0), 5.0, Color32::BROWN, Stroke::default());
+}
+
+
+// display
+const ROUNDING: f32 = 5.0;
+const STROKE_THICKNESS: f32 = 1.0;
+const STROKE_COLOR: Color32 = Color32::GOLD;
+const HEIGHT_BUFFER: f32 = 10.0;
+
+// formating
+const DOT_COUNT: usize = 2;
+const MS_SPACING: usize = 2;
+const LEFT_BUFFER: f32 = 5.0;
+const TEXT_SIZE: f32 = 12.5;
+fn display_segment(
+   ui: &mut Ui,
+   rect: Rect,
+   name: &str,
+   time_ms: f32,
+   color: Color32,
+) -> Response
+{
+   // setup
+   let font = FontId::monospace(TEXT_SIZE);
+   let one_char_size = ui.painter().layout_no_wrap("P".to_string(), font.clone(), Color32::PLACEHOLDER).size();
+
+   // format container
+   let mut con_rect = rect;
+   let tgt_height = one_char_size.y + HEIGHT_BUFFER;
+   let diff = tgt_height - con_rect.height();
+   if diff < 0.0 {
+      con_rect = con_rect.shrink2(Vec2::new(0.0, diff.abs() / 2.0));
+   }
+
+   // draw container
+   ui.painter().rect(
+      con_rect,
+     Rounding::same(ROUNDING),
+     color,
+     Stroke::new(STROKE_THICKNESS, STROKE_COLOR),
+   );
+
+   // input
+   let response = ui.allocate_rect(con_rect.shrink(1.0), Sense {
+      click: true,
+      drag: true,
+      focusable: true,
+   });
+
+   // concat text
+   let one_char_width = one_char_size.x;
+   let size = ui.painter().layout_no_wrap(name.to_string(), font.clone(), Color32::PLACEHOLDER).size();
+
+   let max_size = (size.x + LEFT_BUFFER) + (one_char_width * MS_SPACING as f32) + (one_char_width * ((time_ms as u32).to_string().len() + 5) as f32);
+
+   let mut sized_text = if max_size > rect.width() {
+      let diff = (max_size - rect.width()) + one_char_width * DOT_COUNT as f32;
+      let char_diff = (diff / one_char_width).ceil() as usize;
+
+      let mut txt = name.to_string();
+      if txt.len() < char_diff {
+         txt.clear();
+      }
+      else {
+         txt.drain((txt.len() - char_diff)..txt.len());
+         txt.push_str(".".repeat(DOT_COUNT).as_str());
+         txt.push_str(" ".repeat(MS_SPACING).as_str());
+      }
+      txt
+   }
+   else {
+      let mut txt = name.to_string();
+      txt.push_str(" ".repeat(MS_SPACING).as_str());
+      txt
+   };
+
+   sized_text.push_str(format!("{time_ms:.2}ms").as_str());
+
+   if ((sized_text.len() as f32 * one_char_width) + LEFT_BUFFER) > rect.width() {
+      sized_text = format!("{time_ms:.1}ms");
+   }
+   if ((sized_text.len() as f32 * one_char_width) + LEFT_BUFFER) > rect.width() {
+      sized_text = format!("{time_ms:.0}ms");
+   }
+   if ((sized_text.len() as f32 * one_char_width) + LEFT_BUFFER) > rect.width() {
+      sized_text = format!("{time_ms:.0}");
+   }
+   if ((sized_text.len() as f32 * one_char_width) + LEFT_BUFFER) > rect.width() {
+      sized_text.clear();
+   }
+
+   // draw text
+   ui.painter().text(Pos2::new(rect.min.x + LEFT_BUFFER, rect.center().y), Align2::LEFT_CENTER, sized_text, font, Color32::WHITE);
+
+   response
 }
